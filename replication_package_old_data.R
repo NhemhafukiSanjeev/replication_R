878ebd7b7959885a7168c9a0f01eea190e794916
Nhemhafukisanjeev <125657776+NhemhafukiSanjeev@users.noreply.github.com>
2026-03-04 15:26:36 +0545

    changes

--- replication_package_old_data.R
+++ replication_package_old_data.R
@@ -19,159 +19,157 @@ library(flextable)
 library(modelsummary)
 library(pandoc)
 
-## search for indicators using keywords
-gdp <- WDIsearch("gdp")
-inflation <- WDIsearch("inflation")
-fdi <- WDIsearch("foreign direct investment")
-population <- WDIsearch("labor force")
-
-# Load WDI country metadata (comes built-in with the WDI package)
-country_meta <- WDI_data$country
-
-# Keep only rows where region is NOT "Aggregates"
-countries_only <- country_meta$iso2c[country_meta$region != "Aggregates"]
-
-## select data for concerned variables
-## 1. inflation
-
-inflation_dat <- WDI(
-  country   = "all",          
-  indicator = "FP.CPI.TOTL.ZG", # GDP (current LCU)
-  start     = 1970,
-  end       = 2021
-)
-
-## 2. foreign direct investment
-fdi_dat <- WDI(
-  country   = "all",          
-  indicator = "BX.KLT.DINV.CD.WD", # Foreign direct investment, net inflows (BoP, current US$)
-  start     = 1970,
-  end       = 2021
-)
-
-
-## 3. gdp
-gdp_dat <- WDI(
-  country   = "all",          
-  indicator = "NY.GDP.MKTP.CN", # GDP (current LCU)
-  start     = 1970,
-  end       = 2021
-)
-
-## 4. population
-population_dat <- WDI(
-  country   = "all",          
-  indicator = "SP.POP.TOTL", # Population, total
-  start     = 1970,
-  end       = 2021
-)
-
-# Store all datasets in a named list
-dataset_list <- list(
-  inflation   = inflation_dat,
-  fdi         = fdi_dat,
-  gdp         = gdp_dat,
-  population  = population_dat
-)
-
-## removing unnecessary rows and keeping only necessary rows. Particularly, we 
-## will keep only countries data and not grouped data like Asia, Europe, etc.
-filtered_list <- lapply(dataset_list, function(dat) {
-  dat[dat$iso2c %in% countries_only, ]
-})
-
-# Access individual filtered datasets by name
-inflation_dat_filtered  <- filtered_list$inflation
-fdi_dat_filtered        <- filtered_list$fdi
-gdp_dat_filtered        <- filtered_list$gdp
-population_dat_filtered <- filtered_list$population
-
-# Making a list of the dataset
-filtered_dataset_list <- list(
-  inflation   = inflation_dat_filtered,
-  fdi         = fdi_dat_filtered,
-  gdp         = gdp_dat_filtered,
-  population  = population_dat_filtered
-)
-
-## keeping only the necessary columns in all datasets
-use_only_var_list <- lapply(filtered_dataset_list, function(dat) {
-  dat |> select(-c(iso2c, iso3c))
-})
-
-# Unpack the dataset
-inflation_dat_use_only_var  <- use_only_var_list$inflation
-fdi_dat_use_only_var        <- use_only_var_list$fdi
-gdp_dat_use_only_var        <- use_only_var_list$gdp
-population_dat_use_only_var <- use_only_var_list$population
-
-##renaming the variable
-inflation_renamed <- inflation_dat_use_only_var |> 
-  rename(inf = FP.CPI.TOTL.ZG)
-
-fdi_renamed <- fdi_dat_use_only_var |> 
-  rename(fdi = BX.KLT.DINV.CD.WD)
-
-gdp_renamed <- gdp_dat_use_only_var |> 
-  rename(gdp = NY.GDP.MKTP.CN)
-
-popn_renamed <- population_dat_use_only_var |> 
-  rename(popn = SP.POP.TOTL)
-
-
-## We see NA's in the datasets which are missing values, so we need to remove the
-## NA's from all datasets to be able to have a regression-ready dataset
-## First of all, let's join the dataset
-
-compiled_dat <- inflation_renamed |> 
-  left_join(fdi_renamed,  by = c("country", "year")) |> 
-  left_join(gdp_renamed,  by = c("country", "year")) |> 
-  left_join(popn_renamed, by = c("country", "year"))
-
-## Now, let's remove the NA's.
-na_removed_dat <- compiled_dat |> 
-  na.omit()
-
-## We can't have negative FDI for inflows because that would mean that it's an
-## outflow and not inflow, so we filter out the negative FDI inflows. Also, the 
-## log-transformation, which we'll do later, doesn't accept the negative values,
-## so we remove the negative values
-
-non_negative_fdi <- na_removed_dat |> 
-  filter(fdi > 0)
-
-## Our log-transformation in the regression requires positive values,
-## so negative CPI observations are excluded.
-
-non_negative_cpi <- non_negative_fdi |> 
-  filter(inf > 0)
-
-## We only want to keep those countries which were selected in our former analysis,
-## so we have to filter out those countries. For that we need the list of the 
-## countries that were in the former analysis. 
+# ## search for indicators using keywords
+# gdp <- WDIsearch("gdp")
+# inflation <- WDIsearch("inflation")
+# fdi <- WDIsearch("foreign direct investment")
+# population <- WDIsearch("labor force")
+# 
+# # Load WDI country metadata (comes built-in with the WDI package)
+# country_meta <- WDI_data$country
+# 
+# # Keep only rows where region is NOT "Aggregates"
+# countries_only <- country_meta$iso2c[country_meta$region != "Aggregates"]
+# 
+# ## select data for concerned variables
+# ## 1. inflation
+# 
+# inflation_dat <- WDI(
+#   country   = "all",          
+#   indicator = "FP.CPI.TOTL.ZG", # GDP (current LCU)
+#   start     = 1970,
+#   end       = 2021
+# )
+# 
+# ## 2. foreign direct investment
+# fdi_dat <- WDI(
+#   country   = "all",          
+#   indicator = "BX.KLT.DINV.CD.WD", # Foreign direct investment, net inflows (BoP, current US$)
+#   start     = 1970,
+#   end       = 2021
+# )
+# 
+# 
+# ## 3. gdp
+# gdp_dat <- WDI(
+#   country   = "all",          
+#   indicator = "NY.GDP.MKTP.CN", # GDP (current LCU)
+#   start     = 1970,
+#   end       = 2021
+# )
+# 
+# ## 4. population
+# population_dat <- WDI(
+#   country   = "all",          
+#   indicator = "SP.POP.TOTL", # Population, total
+#   start     = 1970,
+#   end       = 2021
+# )
+# 
+# # Store all datasets in a named list
+# dataset_list <- list(
+#   inflation   = inflation_dat,
+#   fdi         = fdi_dat,
+#   gdp         = gdp_dat,
+#   population  = population_dat
+# )
+# 
+# ## removing unnecessary rows and keeping only necessary rows. Particularly, we 
+# ## will keep only countries data and not grouped data like Asia, Europe, etc.
+# filtered_list <- lapply(dataset_list, function(dat) {
+#   dat[dat$iso2c %in% countries_only, ]
+# })
+# 
+# # Access individual filtered datasets by name
+# inflation_dat_filtered  <- filtered_list$inflation
+# fdi_dat_filtered        <- filtered_list$fdi
+# gdp_dat_filtered        <- filtered_list$gdp
+# population_dat_filtered <- filtered_list$population
+# 
+# # Making a list of the dataset
+# filtered_dataset_list <- list(
+#   inflation   = inflation_dat_filtered,
+#   fdi         = fdi_dat_filtered,
+#   gdp         = gdp_dat_filtered,
+#   population  = population_dat_filtered
+# )
+# 
+# ## keeping only the necessary columns in all datasets
+# use_only_var_list <- lapply(filtered_dataset_list, function(dat) {
+#   dat |> select(-c(iso2c, iso3c))
+# })
+# 
+# # Unpack the dataset
+# inflation_dat_use_only_var  <- use_only_var_list$inflation
+# fdi_dat_use_only_var        <- use_only_var_list$fdi
+# gdp_dat_use_only_var        <- use_only_var_list$gdp
+# population_dat_use_only_var <- use_only_var_list$population
+# 
+# ##renaming the variable
+# inflation_renamed <- inflation_dat_use_only_var |> 
+#   rename(inf = FP.CPI.TOTL.ZG)
+# 
+# fdi_renamed <- fdi_dat_use_only_var |> 
+#   rename(fdi = BX.KLT.DINV.CD.WD)
+# 
+# gdp_renamed <- gdp_dat_use_only_var |> 
+#   rename(gdp = NY.GDP.MKTP.CN)
+# 
+# popn_renamed <- population_dat_use_only_var |> 
+#   rename(popn = SP.POP.TOTL)
+# 
+# 
+# ## We see NA's in the datasets which are missing values, so we need to remove the
+# ## NA's from all datasets to be able to have a regression-ready dataset
+# ## First of all, let's join the dataset
+# 
+# compiled_dat <- inflation_renamed |> 
+#   left_join(fdi_renamed,  by = c("country", "year")) |> 
+#   left_join(gdp_renamed,  by = c("country", "year")) |> 
+#   left_join(popn_renamed, by = c("country", "year"))
+# 
+# ## Now, let's remove the NA's.
+# na_removed_dat <- compiled_dat |> 
+#   na.omit()
+# 
+# ## We can't have negative FDI for inflows because that would mean that it's an
+# ## outflow and not inflow, so we filter out the negative FDI inflows. Also, the 
+# ## log-transformation, which we'll do later, doesn't accept the negative values,
+# ## so we remove the negative values
+# 
+# non_negative_fdi <- na_removed_dat |> 
+#   filter(fdi > 0)
+# 
+# ## Our log-transformation in the regression requires positive values,
+# ## so negative CPI observations are excluded.
+# 
+# non_negative_cpi <- non_negative_fdi |> 
+#   filter(inf > 0)
+# 
+# ## We only want to keep those countries which were selected in our former analysis,
+# ## so we have to filter out those countries. For that we need the list of the 
+# ## countries that were in the former analysis. 
 
 file_path <- "D:/Research/Does FDI have negative relationship with Inflation/replication_R/replication_R/"
 former_analysis_dat <- read_excel(paste0(file_path, "Final_data.xlsx"), sheet = "Final_data")
 
-selected_country_year <- former_analysis_dat |> 
-  select(Country, year)
+# selected_country_year <- former_analysis_dat |> 
+#   select(Country, year)
 
 ## Now that we have the year and country that was used in the former data analysis,
 ## we can now use the data for those countries and period by joining the two dataset.
 
-data_analysis_new <- selected_country_year |> 
-  left_join(non_negative_cpi, by = c("Country" = "country", "year"))
+# data_analysis_new <- selected_country_year |> 
+#   left_join(non_negative_cpi, by = c("Country" = "country", "year"))
 
 ## Now that we have exactly the same countries and year as our former analysis,
 ## we now proceed to log_transform our variables
 
-final_dat <- data_analysis_new |> 
-  mutate(ln_cpi  = log(inf),
-         ln_fdi  = log(fdi),
-         ln_gdp  = log(gdp),
-         ln_popn = log(popn)) |> 
-  select(-c("inf", "fdi", "gdp", "popn"))
-
+final_dat <- former_analysis_dat |> 
+  rename(ln_cpi  = Log_inf,
+         ln_fdi  = Log_FDI,
+         ln_gdp  = Log_gdp,
+         ln_popn = Log_Pop) 
 
 ## Now that we have analysis ready data, we now prepare for the components of the
 ## data analysis
@@ -235,7 +233,7 @@ ft <- flextable(summary_table) |>
   autofit()
 
 # Save to Word
-save_as_docx(ft, path = "summary_stats.docx")
+save_as_docx(ft, path = "summary_stats_old_dat.docx")
 
 ## 2. Correlation Matrix
 
@@ -288,7 +286,7 @@ ft <- flextable(cor_table) |>
   autofit()
 
 # Export to Word
-save_as_docx(ft, path = "correlation_matrix.docx")
+save_as_docx(ft, path = "correlation_matrix_old_dat.docx")
 
 ## Now let's move on to regression
 
@@ -345,7 +343,7 @@ reg_table <- modelsummary(
   hline_bottom(part = "body",   border = officer::fp_border(width = 2)) |>
   autofit()
 
-save_as_docx(reg_table, path = "regression_table.docx")
+save_as_docx(reg_table, path = "regression_table_old_dat.docx")
 
 
 ## save in word
